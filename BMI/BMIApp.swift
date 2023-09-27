74ae128ba2810408194c580531dfe4733449350d
PandaKris <kristantosean@gmail.com>
2023-09-27 07:54:13 +0700

    Initial Commit

---
+++ BMI/BMIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMIApp.swift
+//  BMI
+//
+//  Created by Kristanto Sean on 27/09/23.
+//
+
+import SwiftUI
+
+@main
+struct BMIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
